fe52374a99a05c391584c3a1e2709a0b5ae8b94c
tenclar <tenclar.silva@ac.gov.br>
2020-11-17 14:04:38 -0500

    ajuste de referencia

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
@@ -28,7 +29,7 @@ export default {
     storage: multer.diskStorage({
       destination: tmpFolder,
       filename(request, file, callback) {
-        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileHash = crypto.randomBytes(10).toString('hex');
         const fileName = `${fileHash}-${file.originalname}`;
         return callback(null, fileName);
       },
