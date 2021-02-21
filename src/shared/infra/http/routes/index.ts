ede22ef57ddd3db484c4be22bf2d57bf6f83ae9e
tenclar <tenclarvalus@gmail.com>
2021-02-20 21:57:18 -0500

    rotas para cidade, orgaos

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -7,6 +7,8 @@ import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import categoriasRouter from '@modules/categorias/infra/http/routes/categorias.routes';
 import servicosRouter from '@modules/servicos/infra/http/routes/servicos.routes';
 import cidadesRouter from '@modules/cidades/infra/http/routes/cidades.routes';
+import orgaosRouter from '@modules/orgaos/infra/http/routes/orgaos.routes';
+import superioresRouter from '@modules/superiores/infra/http/routes/superiores.routes';
 
 const routes = Router();
 
@@ -20,6 +22,8 @@ routes.use('/users', usersRouter);
 routes.use('/profile', profileRouter);
 routes.use('/categorias', categoriasRouter);
 routes.use('/servicos', servicosRouter);
+routes.use('/superiores', superioresRouter);
 routes.use('/cidades', cidadesRouter);
+routes.use('/orgaos', orgaosRouter);
 
 export default routes;
