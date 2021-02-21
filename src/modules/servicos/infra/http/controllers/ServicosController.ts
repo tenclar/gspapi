b418bc7e2d11d77b0e181bf5d54267c7a896e4b8
tenclar <tenclarvalus@gmail.com>
2021-02-20 23:51:58 -0500

    inclusao de novos campos servicos

--- src/modules/servicos/infra/http/controllers/ServicosController.ts
+++ src/modules/servicos/infra/http/controllers/ServicosController.ts
@@ -4,15 +4,19 @@ import { container } from 'tsyringe';
 
 import CreateServicoService from '@modules/servicos/services/CreateServicosService';
 import ListServicoService from '@modules/servicos/services/ListServicosService';
+import ShowServicosService from '@modules/servicos/services/ShowServicosService';
+import UpdateServicosService from '@modules/servicos/services/UpdateServicosService';
 
 export default class ServicosController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { titulo, informacao } = request.body;
+    const { titulo, informacao, orgao_id, categoria_id } = request.body;
 
     const createServico = container.resolve(CreateServicoService);
     const servico = await createServico.execute({
       titulo,
       informacao,
+      orgao_id,
+      categoria_id,
     });
 
     return response.json({ servico: classToClass(servico) });
@@ -22,5 +26,28 @@ export default class ServicosController {
     const listServicos = await container.resolve(ListServicoService);
     const servicos = await listServicos.execute();
     return response.json({ servicos: classToClass(servicos) });
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showServico = container.resolve(ShowServicosService);
+    const servico = await showServico.execute({ id });
+    return response.json({ servico: classToClass(servico) });
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    // const { id } = request.params;
+    const { id, titulo, informacao, orgao_id, categoria_id } = request.body;
+    const updateServicos = container.resolve(UpdateServicosService);
+
+    const servico = await updateServicos.execute({
+      id,
+      titulo,
+      informacao,
+      orgao_id,
+      categoria_id,
+    });
+
+    return response.json({ servico: classToClass(servico) });
   }
 }
