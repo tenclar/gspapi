a6ac245a77514413c8cf5d6bf9428ba2e37a079a
tenclar <tenclarvalus@gmail.com>
2021-02-20 19:29:55 -0500

    rotas

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,6 +6,7 @@ import passwordRouter from '@modules/users/infra/http/routes/passowords.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import categoriasRouter from '@modules/categorias/infra/http/routes/categorias.routes';
 import servicosRouter from '@modules/servicos/infra/http/routes/servicos.routes';
+import cidadesRouter from '@modules/cidades/infra/http/routes/cidades.routes';
 
 const routes = Router();
 
@@ -19,5 +20,6 @@ routes.use('/users', usersRouter);
 routes.use('/profile', profileRouter);
 routes.use('/categorias', categoriasRouter);
 routes.use('/servicos', servicosRouter);
+routes.use('/cidades', cidadesRouter);
 
 export default routes;
