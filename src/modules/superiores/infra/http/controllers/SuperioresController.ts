79e2efac439f0c9ba765980f3e8c92f34d18540d
tenclar <tenclarvalus@gmail.com>
2021-02-20 21:56:43 -0500

    Modulo Instituicoes superiores

---
+++ src/modules/superiores/infra/http/controllers/SuperioresController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
+import { container } from 'tsyringe';
+import CreateSuperioresService from '@modules/superiores/services/CreateSuperioresService';
+import ShowSuperioreservice from '@modules/superiores/services/ShowSuperioresService';
+import ListSuperioresService from '@modules/superiores/services/ListSuperioresService';
+import UpdateSuperioresService from '@modules/superiores/services/UpdateSuperioresService';
+
+export default class SuperioresController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { nome } = request.body;
+    const CreateSuperiores = container.resolve(CreateSuperioresService);
+    const superior = await CreateSuperiores.execute({ nome });
+    return response.json({ orgao: classToClass(superior) });
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showSuperiores = container.resolve(ShowSuperioreservice);
+    const orgao = await showSuperiores.execute({ id });
+    return response.json({ orgao: classToClass(orgao) });
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listSuperiores = await container.resolve(ListSuperioresService);
+    const superiores = await listSuperiores.execute();
+    return response.json({ superiores: classToClass(superiores) });
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    // const { id } = request.params;
+    const { id, nome } = request.body;
+    const updateSuperiores = container.resolve(UpdateSuperioresService);
+
+    const superior = await updateSuperiores.execute({
+      id,
+      nome,
+    });
+
+    return response.json({ superior: classToClass(superior) });
+  }
+}
---
+++ src/modules/superiores/infra/http/routes/superiores.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+import SuperioresController from '../controllers/SuperioresController';
+
+const superioresRouter = Router();
+
+const superioresController = new SuperioresController();
+
+superioresRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      nome: Joi.string().required(),
+    },
+  }),
+  superioresController.create,
+);
+superioresRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      id: Joi.string().required(),
+      nome: Joi.string().required(),
+    },
+  }),
+  superioresController.update,
+);
+superioresRouter.get('/', superioresController.index);
+
+superioresRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  superioresController.show,
+);
+export default superioresRouter;
