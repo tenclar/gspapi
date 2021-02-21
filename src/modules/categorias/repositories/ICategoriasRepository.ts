8fb718981f0f63eb962f7c8b726db52c2d7cfee2
tenclar <tenclarvalus@gmail.com>
2021-02-20 19:27:57 -0500

    Categorias repository

--- src/modules/categorias/repositories/ICategoriasRepository.ts
+++ src/modules/categorias/repositories/ICategoriasRepository.ts
@@ -1,7 +1,7 @@
 import ICreateCategoriaDTO from '@modules/categorias/dtos/ICreateCategoriaDTO';
 import Categoria from '@modules/categorias/infra/typeorm/entities/Categoria';
 
-export default interface ICategoriasReposotory {
+export default interface ICategoriasRepository {
   create(data: ICreateCategoriaDTO): Promise<Categoria>;
   save(categoria: Categoria): Promise<Categoria>;
   findById(id: string): Promise<Categoria | undefined>;
