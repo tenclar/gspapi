010ef07b0a7b529859ee9b35b7d8d7f385944fea
José Jardim <nosfcj@gmail.com>
2021-03-02 09:55:16 -0500

    Nova tabela Avisos 02032022

--- src/shared/infra/typeorm/migrations/1614367910411-AddTableAviso.ts
+++ src/shared/infra/typeorm/migrations/1614367910411-AddTableAviso.ts
@@ -4,7 +4,7 @@ export default class AddTableAviso1614367910411 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'cidades',
+        name: 'avisos',
         columns: [
           {
             name: 'id',
