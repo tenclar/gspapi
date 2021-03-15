4e180a4a45df90ec05ff6e5183f6a3f6ca4d7453
tenclar <tenclarvalus@gmail.com>
2021-03-14 21:33:03 -0500

    tipagem para lista de publicos

--- src/modules/servicos/dtos/ICreateServicoDTO.ts
+++ src/modules/servicos/dtos/ICreateServicoDTO.ts
@@ -1,7 +1,12 @@
-export default interface ICreateSerrvicoDTO {
+interface IPublico {
+  publico_id: string;
+}
+
+export default interface ICreateServicoDTO {
   titulo: string;
   slug: string;
   informacao: string;
   orgao_id: string;
   categoria_id: string;
+  publicos: IPublico[];
 }
